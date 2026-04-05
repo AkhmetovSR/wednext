6d7e03774e0904acaf6fcbcd48e714a10d367ef7
ahmetov <ahmetov26@gmail.com>
2026-04-06 00:22:39 +0500

    05042026h

--- components/CarouselSwipe/Swipe.jsx
+++ components/CarouselSwipe/Swipe.jsx
@@ -13,7 +13,7 @@ import { getSlidePosition } from '@/utils/slidePosition';
 import {useSlideSync} from "@/hooks/useSlideSync";
 import Lottie from 'lottie-react';
 import animationData from '@/public/lottie/Swipe.json';
-import tapAnimation from '@/public/lottie/Tap.json';
+import tapAnimation from '@/public/lottie/tap.json';
 
 const Swipe = ({ children }) => {
     const { setBb, activeSlide, setActiveSlide, autoSlide, setAutoSlide, clickAnimation } = useCarouselState();
