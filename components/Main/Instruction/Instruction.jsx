42acf6b124d06d89a44a158ce64c1ee9c0641df9
ahmetov <ahmetov26@gmail.com>
2026-04-12 13:03:23 +0500

    12042026h

--- components/Main/Instruction/Instruction.jsx
+++ components/Main/Instruction/Instruction.jsx
@@ -8,14 +8,24 @@ export default function Instruction() {
     return (
         <div className={main.Main}>
             <div className={s.Cont}>
-                <h4>Как создать свое онлайн-приглашение на свадьбу</h4>
-                <div className={s.ImgInstr}><Image className={s.Img} src="/images/Instr1.webp" alt="Instruction 1" width={290} height={600} loading="eager"/></div>
-                <br/>
-                <div className={s.ImgInstr}><Image className={s.Img} src="/images/Instr2.webp" alt="Instruction 2" width={290} height={600}/></div>
-                <br/>
-                <div className={s.ImgInstr}><Image className={s.Img} src="/images/Instr3.webp" alt="Instruction 3" width={290} height={600}/></div>
-                <br/>
-                <div className={s.ImgInstr}><Image className={s.Img} src="/images/Instr4.webp" alt="Instruction 4" width={290} height={600}/></div>
+                <div className={s.Title}>Инструкция</div>
+                <h1 className={s.InstTitle}>Как создать свое свадебное пригласительное онлайн</h1>
+                <span className={s.P1}>1. Выберите понравившийся ❤️ шаблон</span>
+                <div className={s.Temp}>
+                    <div className={s.T1}></div>
+                    <div className={s.T2}>
+                        <div>« --- »</div>
+                        <div>👆</div>
+                    </div>
+                    <div className={s.T3}></div>
+                </div>
+                {/*<div className={s.ImgInstr}><Image className={s.Img} src="/images/Instr1.webp" alt="Instruction 1" width={290} height={600} loading="eager"/></div>*/}
+                {/*<br/>*/}
+                {/*<div className={s.ImgInstr}><Image className={s.Img} src="/images/Instr2.webp" alt="Instruction 2" width={290} height={600}/></div>*/}
+                {/*<br/>*/}
+                {/*<div className={s.ImgInstr}><Image className={s.Img} src="/images/Instr3.webp" alt="Instruction 3" width={290} height={600}/></div>*/}
+                {/*<br/>*/}
+                {/*<div className={s.ImgInstr}><Image className={s.Img} src="/images/Instr4.webp" alt="Instruction 4" width={290} height={600}/></div>*/}
                 <br/>
             </div>
         </div>
